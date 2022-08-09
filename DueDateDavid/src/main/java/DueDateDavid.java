a4e7736f17ae31ac8579bc2ce9c7a03545b76ab2
Braeden Windle <braeden.windle@gmail.com>
2022-08-09 21:21:17 +1200

    Changed method for getting token to .env

--- DueDateDavid/src/main/java/DueDateDavid.java
+++ DueDateDavid/src/main/java/DueDateDavid.java
@@ -1,14 +1,17 @@
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import io.github.cdimascio.dotenv.Dotenv;
 import reactor.core.publisher.Mono;
+
 
 public class DueDateDavid {
     /**
      * Runs the bots
-     * @param args First arugment should be the token
+     * @param args First argument should be the token
      */
     public static void main(String[] args){
-        DiscordClient client = DiscordClient.create(args[0]);
+        Dotenv dotenv = Dotenv.load();
+        DiscordClient client = DiscordClient.create(dotenv.get("TOKEN"));
 
         Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
 
