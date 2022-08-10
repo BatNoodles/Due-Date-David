29736e1f768485f70856fb45b135cde1aa6bd0b8
Braeden Windle <braeden.windle@gmail.com>
2022-08-11 00:34:20 +1200

    Outlined data structures for due dates and subjects

---
+++ DueDateDavid/src/main/java/dueDates/DueDate.java
@@ -0,0 +1,13 @@
+package dueDates;
+
+import java.beans.Transient;
+import java.util.Date;
+
+public class DueDate {
+    private String name;
+    private Subject subject;
+    private Date date;
+
+    public DueDate(String name, String subject, int day, int month, int hour) {
+    }
+}
---
+++ DueDateDavid/src/main/java/dueDates/Subject.java
@@ -0,0 +1,8 @@
+package dueDates;
+
+import java.util.List;
+
+public class Subject {
+    private String name;
+    private List<Long> memberIds;
+}
