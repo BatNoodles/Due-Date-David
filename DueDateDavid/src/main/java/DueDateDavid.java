50aa59b495fd9da6965b9b90ed221be0c9c1bf6b
Braeden Windle <braeden.windle@gmail.com>
2022-08-11 00:33:53 +1200

    Bot now handles ChatInputInteractionEvents, provided there a valid SlashCommand class

--- DueDateDavid/src/main/java/DueDateDavid.java
+++ DueDateDavid/src/main/java/DueDateDavid.java
@@ -1,5 +1,6 @@
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.List;
@@ -18,6 +19,6 @@ public class DueDateDavid {
             System.out.println("Error trying to update slash commands: " + e);
         }
 
-        client.onDisconnect().block();
+        client.on(ChatInputInteractionEvent.class, SlashCommandHandler::handleCommand).then(client.onDisconnect()).block();
     }
 }
