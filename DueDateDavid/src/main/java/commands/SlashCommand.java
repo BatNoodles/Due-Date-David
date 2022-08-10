5894b7f8414456855faf8fa6702c3b09beeb80bd
Braeden Windle <braeden.windle@gmail.com>
2022-08-11 00:26:31 +1200

    Added slash command interface

---
+++ DueDateDavid/src/main/java/commands/SlashCommand.java
@@ -0,0 +1,9 @@
+package commands;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import reactor.core.publisher.Mono;
+
+public interface SlashCommand {
+    String name();
+    Mono<Void> handleCommand(ChatInputInteractionEvent event);
+}
