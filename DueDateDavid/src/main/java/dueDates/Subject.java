9d38e15b4a429c6c09a8a5f54a95b6fa76710a09
Braeden Windle <braeden.windle@gmail.com>
2022-08-12 01:13:17 +1200

    Added constructor & a getter + setter for use in database.

--- DueDateDavid/src/main/java/dueDates/Subject.java
+++ DueDateDavid/src/main/java/dueDates/Subject.java
@@ -1,11 +1,20 @@
 package dueDates;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Data structure to represent a subject. Has a subject name and a list of discord users "enrolled" in that class.
  */
 public class Subject {
-    private String name;
-    private List<Long> memberIds;
+    private final String name;
+    private final Set<Long> memberIds;
+
+    public Subject(String name){
+        this.name = name;
+        memberIds = new HashSet<>();
+    }
+    public String getName(){return name;}
+
+    public void addUserId(Long userId){memberIds.add(userId);}
 }
