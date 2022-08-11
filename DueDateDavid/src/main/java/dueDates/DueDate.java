1e5a060ce813890b2f4ad74d86af236c77b3ae72
Braeden Windle <braeden.windle@gmail.com>
2022-08-12 01:44:28 +1200

    Slight update to due date. Still unsure if this is how I want to implement it.

--- DueDateDavid/src/main/java/dueDates/DueDate.java
+++ DueDateDavid/src/main/java/dueDates/DueDate.java
@@ -6,11 +6,16 @@ import java.util.Date;
 /**
  * A data structure to represent a due date. Has a name, subject and date/time that it is due.
  */
+
+
+//TODO: Decide whether the date should be inputted as a string (and then checked) or keep it as a number. same with time?
+
 public class DueDate {
     private String name;
     private Subject subject;
     private Date date;
 
     public DueDate(String name, String subject, int day, int month, int hour) {
+        this.name = name;
     }
 }
