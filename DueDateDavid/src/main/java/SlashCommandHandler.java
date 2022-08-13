349518000af21c216b0b9cba1e68bac29468bcce
Braeden Windle <braeden.windle@gmail.com>
2022-08-14 00:39:51 +1200

    Added JoinCommand to the list of commands handled in the command handler

--- DueDateDavid/src/main/java/SlashCommandHandler.java
+++ DueDateDavid/src/main/java/SlashCommandHandler.java
@@ -1,4 +1,5 @@
 import commands.AddCommand;
+import commands.JoinCommand;
 import commands.SlashCommand;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import reactor.core.publisher.Mono;
@@ -10,7 +11,7 @@ import java.util.List;
  */
 public class SlashCommandHandler {
 
-    private final static List<SlashCommand> commands = List.of(new AddCommand());
+    private final static List<SlashCommand> commands = List.of(new AddCommand(), new JoinCommand());
 
     public static Mono<Void> handleCommand(ChatInputInteractionEvent e){
         return commands.stream().filter(c -> c.name().equals(e.getCommandName())).map(c-> c.handleCommand(e)).findFirst().orElse(Mono.empty());
