6995add22717af620b25e588f933070e2abf0f6b
Braeden Windle <braeden.windle@gmail.com>
2022-08-18 02:02:25 +1200

    DueDateReminderHandler.handleRemind is now called every minute.

--- DueDateDavid/src/main/java/DueDateDavid.java
+++ DueDateDavid/src/main/java/DueDateDavid.java
@@ -2,11 +2,13 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import io.github.cdimascio.dotenv.Dotenv;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
 
-//TODO: add a listener every minute to check if a due date is happening in the next hour. Also add a recurring event every morning and every monday morning (for daily and weekly reminders of due dates)
 
 public class DueDateDavid {
     public static void main(String[] args){
@@ -19,6 +21,10 @@ public class DueDateDavid {
         catch (Exception e){
             System.out.println("Error trying to update slash commands: " + e);
         }
+
+
+        Mono.just(client).repeatWhen(fluxDelay -> Flux.interval(Duration.ofSeconds(60))).subscribe(DueDateReminderHandler::handleRemind);
+
 
         client.on(ChatInputInteractionEvent.class, SlashCommandHandler::handleCommand).then(client.onDisconnect()).block(); //Listens for SlashCommands used in the chat until the bot disconnects
     }
