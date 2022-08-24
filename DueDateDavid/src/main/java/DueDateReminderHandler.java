156f76c3ae35f292e4cd1bd4ef8adf31e75efc52
Braeden Windle <braeden.windle@gmail.com>
2022-08-24 12:11:29 +1200

    Changed DueDateReminderHandler filtering to use the DataBase.filterDueDates method.

--- DueDateDavid/src/main/java/DueDateReminderHandler.java
+++ DueDateDavid/src/main/java/DueDateReminderHandler.java
@@ -8,14 +8,10 @@ import dueDates.Database;
 public class DueDateReminderHandler {
     public static void handleRemind(GatewayDiscordClient client){
         Database database = Database.getInstance();
-//        database.getDueDates().forEach(dueDate -> System.out.println("Time until due date: " + dueDate.getTimeUntil().toHoursPart())); //Temporary, for debugging
-        database.getDueDates()
-                .stream()
-                .filter(d -> d.getTimeUntil().toHoursPart() == 1).
-                forEach(dueDate ->
-                        client.getChannelById(Snowflake.of(445386053042307072L))
-                                .subscribe(channel -> channel.getRestChannel().createMessage("The following is due in one hour. Make sure to submit!\n" + dueDate).subscribe())
-                );
+
+        database.filterDueDates(d -> d.getTimeUntil().toHoursPart() == 1).forEach(dueDate ->
+                client.getChannelById(Snowflake.of(445386053042307072L)) //Hardcoded id, will be changed later.
+                        .subscribe(channel -> channel.getRestChannel().createMessage("The following is due in one hour. Make sure to submit!\n" + dueDate).subscribe()));
 
     }
 }
