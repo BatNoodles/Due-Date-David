3f93aefa3a98b8cc0fb63e4f392ae3e553d1f2b5
Braeden Windle <braeden.windle@gmail.com>
2022-08-26 23:19:07 +1200

    Commands cannot have uppercase in name, rethinking how to do showCourse

--- DueDateDavid/src/main/java/SlashCommandHandler.java
+++ DueDateDavid/src/main/java/SlashCommandHandler.java
@@ -12,7 +12,14 @@ public class SlashCommandHandler {
     private final static List<SlashCommand> commands = List.of(new AddCommand(), new JoinCommand(), new ShowCommand());
 
     public static Mono<Void> handleCommand(ChatInputInteractionEvent e){
-        return commands.stream().filter(c -> c.name().equals(e.getCommandName())).map(c-> c.handleCommand(e)).findFirst().orElse(Mono.empty());
+        return commands
+                .stream()
+                .filter(c -> c
+                        .name()
+                        .equals(e.getCommandName()))
+                .map(c-> c.handleCommand(e))
+                .findFirst()
+                .orElse(Mono.empty());
     }
 
 
