68a41084e65f692ed531cdeb4a35c6ad0527709d
Braeden Windle <braeden.windle@gmail.com>
2022-08-27 00:00:51 +1200

    Added javadocs to SlashCommand

--- DueDateDavid/src/main/java/commands/SlashCommand.java
+++ DueDateDavid/src/main/java/commands/SlashCommand.java
@@ -7,6 +7,16 @@ import reactor.core.publisher.Mono;
  * Representation of a slash command.
  */
 public interface SlashCommand {
+    /**
+     * Gets the name of the slash command
+     * @return String - Name
+     */
     String name();
+
+    /**
+     * Handles the slash command
+     * @param event The slash command sent by the user
+     * @return The action to be taken
+     */
     Mono<Void> handleCommand(ChatInputInteractionEvent event);
 }
