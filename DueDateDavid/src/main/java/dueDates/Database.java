a62b48712b6182bf5366d6054add5980056f9c1f
Braeden Windle <braeden.windle@gmail.com>
2022-09-04 05:29:42 +1200

    Added save and load methods in Database

--- DueDateDavid/src/main/java/dueDates/Database.java
+++ DueDateDavid/src/main/java/dueDates/Database.java
@@ -1,18 +1,32 @@
 package dueDates;
 
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Singleton database.
  */
 public class Database {
+
+    private static final Logger logger = Logger.getLogger(Database.class.getName());
+
     private final List<DueDate> dueDates;
     private final List<Course> courses;
 
-    private static final Database DATABASE = new Database();
+    private static final String FILENAME = "resources/database.json";
+
+    private static final Database DATABASE = loadOrNew(FILENAME);
+
 
     public static Database getInstance(){
         return DATABASE;
@@ -22,6 +36,7 @@ public class Database {
         dueDates = new ArrayList<>();
         courses = new ArrayList<>();
     }
+
 
     public List<DueDate> getDueDates(){return Collections.unmodifiableList(dueDates);}
 
@@ -35,7 +50,8 @@ public class Database {
     public List<DueDate> filterDueDates(Predicate<? super DueDate> predicate){return dueDates.stream().filter(predicate).toList();}
 
     public void addCourse(String courseName){
-        courses.add(new Course(courseName));}
+        courses.add(new Course(courseName));
+    }
 
     public void addDueDate(DueDate dueDate){dueDates.add(dueDate);}
 
@@ -76,5 +92,53 @@ public class Database {
     public boolean userInCourse(String course, Long userId){
         return getCourse(course).map(value -> value.containsUser(userId)).orElse(false);
     }
+
+    /**
+     * Saves the Database to a json file.
+     * @param filename - Filename to be saved to.
+     * @throws IOException - Error was encountered saving the file.
+     */
+    public void save(String filename) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("DueDateSerializer", new Version(1,0,0,null,null,null));
+        module.addSerializer(DueDate.class, new DueDate.DueDateSerializer());
+
+        objectMapper.registerModule(module);
+
+        objectMapper.writeValue(new File(filename), getInstance());
+    }
+
+    /**
+     * Attempts to load a Database from a json file. If there is an error a new, empty database is created.
+     * @param filename - Filename of the json file.
+     * @return Database - the new Database loaded.
+     */
+    private static Database loadOrNew(String filename){
+        try {
+            return load(filename);
+        } catch (IOException e) {
+            logger.log(Level.INFO, e + ": Creating empty database");
+            return new Database();
+        }
+    }
+
+    /**
+     * Loads a database from a json file.
+     * @param filename - Name of the json file.
+     * @return Database - the database loaded
+     * @throws IOException - Error is encountered opening the file.
+     */
+    public static Database load(String filename) throws IOException{
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("DueDateDeserializer", new Version(1,0,0,null,null,null));
+        module.addDeserializer(DueDate.class, new DueDate.DueDateDeserializer());
+
+        objectMapper.registerModule(module);
+
+        return objectMapper.readValue(new File(filename), Database.class);
+    }
+
+
 
 }
