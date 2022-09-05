7b3ff85444f864c12b8e2619b437aeddf62fdbe6
Braeden Windle <braeden.windle@gmail.com>
2022-09-05 13:52:39 +1200

    Added test for serializing an empty Database

--- DueDateDavid/src/test/java/dueDates/DatabaseSerializationTests.java
+++ DueDateDavid/src/test/java/dueDates/DatabaseSerializationTests.java
@@ -2,7 +2,10 @@ package dueDates;
 
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class DatabaseSerializationTests {
@@ -11,7 +14,19 @@ public class DatabaseSerializationTests {
     public void testExceptionWhenFileNotFound(){
         Exception exception = assertThrows(IOException.class, ()-> Database.load("NONEXISTENTFILE"));
         assertTrue(exception.getMessage().contains("cannot find the file specified"));
+    }
 
+
+    @Test
+    public void testSerializeEmptyDatabase() throws IOException {
+        Database database = new Database();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        database.serialize(outputStream);
+
+        String output = outputStream.toString();
+
+        assertEquals("{\"dueDates\":[],\"courses\":[]}", output);
     }
 
 }
