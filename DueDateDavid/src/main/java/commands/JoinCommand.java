c6d739ff4113c0d9dbf0f9540ba67c95eabf71e4
Braeden Windle <braeden.windle@gmail.com>
2022-09-06 19:40:46 +1200

    JoinCommand now saves the database when adding a due date. It also will send a reply if there was an IOException.

--- DueDateDavid/src/main/java/commands/JoinCommand.java
+++ DueDateDavid/src/main/java/commands/JoinCommand.java
@@ -4,6 +4,8 @@ import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import dueDates.Database;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+
 public class JoinCommand implements SlashCommand{
 
     @Override
@@ -21,6 +23,12 @@ public class JoinCommand implements SlashCommand{
         if(database.userInCourse(course, userId)) return event.reply("You are already in " + course).withEphemeral(true);
 
         database.joinCourse(course, userId);
+        try {
+            Database.save();
+        } catch (IOException e) {
+            return event.reply("You have joined the course but there was an error saving to disk. Please contact the developer.").withEphemeral(true);
+        }
+
         return event.reply("Successfully joined the course " + course).withEphemeral(true);
     }
 }
