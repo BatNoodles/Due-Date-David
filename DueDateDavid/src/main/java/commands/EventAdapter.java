b1383b307d9dd531b6630cf0802d2f85d677bf9c
Braeden Windle <braeden.windle@gmail.com>
2022-09-07 19:09:45 +1200

    Made comments and methods names of EventAdapter more clear.

--- DueDateDavid/src/main/java/commands/EventAdapter.java
+++ DueDateDavid/src/main/java/commands/EventAdapter.java
@@ -7,17 +7,32 @@ import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
  * Hides the ugly part of getting options from a ChatInputInteraction event. Has no null checking so this only works for options that are required.
  */
 public class EventAdapter {
-    private ChatInputInteractionEvent event;
+    private final  ChatInputInteractionEvent event;
 
     public EventAdapter(ChatInputInteractionEvent event){
         this.event = event;
     }
 
+    /**
+     * Gets the ApplicationCommandInteractionOptionValue of a specified string.
+     * @param name - String name of the option.
+     * @return the value of the specified option.
+     */
     private ApplicationCommandInteractionOptionValue get(String name){
         return event.getOption(name).get().getValue().get();
     }
 
+    /**
+     * Gets the option as a string.
+     * @param name - String name of the option.
+     * @return String value of option
+     */
     public String optionAsString(String name){return get(name).asString();}
 
-    public Long optionAsInt(String name){return get(name).asLong();}
+    /**
+     * Gets the option as a long.
+     * @param name - String name of the option.
+     * @return Long value of the option.
+     */
+    public Long optionAsLong(String name){return get(name).asLong();}
 }
