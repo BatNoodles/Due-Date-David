66b751474a5a431d4656fbc74a27a01b01949040
Braeden Windle <braeden.windle@gmail.com>
2022-09-08 00:55:34 +1200

    Fixed RemoveCommand as Database.getDueDates returns an unmodifiable collection.

--- DueDateDavid/src/main/java/commands/RemoveCommand.java
+++ DueDateDavid/src/main/java/commands/RemoveCommand.java
@@ -2,7 +2,6 @@ package commands;
 
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import dueDates.Database;
-import dueDates.DueDate;
 import reactor.core.publisher.Mono;
 
 public class RemoveCommand implements SlashCommand{
@@ -15,14 +14,11 @@ public class RemoveCommand implements SlashCommand{
     public Mono<Void> handleCommand(ChatInputInteractionEvent event) {
         EventAdapter eventAdapter = new EventAdapter(event);
         int index = Math.toIntExact(eventAdapter.optionAsLong("index"));
-
         Database database = Database.getInstance();
 
         if (database.getDueDates().size() <= index || index  < 0) return event.reply("That is not a valid index! Use */show* to make sure you have the correct index.").withEphemeral(true);
-        DueDate date = database.getDueDates().get(index);
-        database.getDueDates().remove(index);
 
-        return event.reply(date + " was removed.").withEphemeral(true);
+        return event.reply(database.removeDueDate(index) + " was removed.").withEphemeral(true);
 
     }
 }
